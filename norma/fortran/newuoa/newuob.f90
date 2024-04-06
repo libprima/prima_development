faa999d7a74c56089dfed3480bb823187dc8f985
zaikunzhang <zaikunzhang@gmail.com>
2024-04-06 23:54:23 +0800

    240406.235423.HKT fortran/newuoa: revise the threshold for shiftbase in newuob.f90 from 5.0E2 to 1.0E2 and the threshold for model scaling in trustregion.f90 from 1.0E10 to 1.0E12; without this, the performance compared with `archiva` is bad when `RP = REAL32`

--- norma/fortran/newuoa/newuob.f90
+++ norma/fortran/newuoa/newuob.f90
@@ -8,7 +8,7 @@ module newuob_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Thursday, April 04, 2024 PM10:27:09
+! Last Modified: Saturday, April 06, 2024 PM11:48:25
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -620,7 +620,8 @@ do tr = 1, maxtr
     ! 1. After a trust region step that is not short, shift XBASE if SUM(XOPT**2) >= 1.0E3*DNORM**2.
     ! 2. Before a geometry step, shift XBASE if SUM(XOPT**2) >= 1.0E3*DELBAR**2.
     !if (sum(xpt(:, kopt)**2) >= 1.0E2_RP * delta**2) then  ! 1.0E2 works better than 1.0E3 on 20230227.
-    if (sum(xpt(:, kopt)**2) >= 5.0E2_RP * delta**2) then  ! 1.0E2 works better than 1.0E3 on 20230227.
+    !if (sum(xpt(:, kopt)**2) >= 5.0E2_RP * delta**2) then  ! 1.0E2 works better than 1.0E3 on 20230227.
+    if (sum(xpt(:, kopt)**2) >= 1.0E2_RP * delta**2) then  ! 1.0E2 works better than 1.0E3 on 20230227.
         call shiftbase(kopt, xbase, xpt, zmat, bmat, pq, hq, idz)
     end if
 end do  ! End of DO TR = 1, MAXTR. The iterative procedure ends.
